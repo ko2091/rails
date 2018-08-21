b45d3e25c6cbda684f2849023bd60490a98e2fe0
kou <kosukeohen@icloud.com>
2018-08-21 05:30:26 +0000

    save mondai

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,28 @@
- require 'test_helper'
- 
- class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+require 'test_helper'
 
-   test "should get home" do
-   get root_path
-     assert_response :success
-   assert_select "title", "Ruby on Rails Tutorial Sample App"
-   end
- 
-   test "should get help" do
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
     get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-   get about_path
-
-     assert_response :success
+    get about_path
+    assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-   end
- 
+  end
+
   test "should get contact" do
     get contact_path
-   assert_response :success
+    assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
-  end
-\ No newline at end of file
+end

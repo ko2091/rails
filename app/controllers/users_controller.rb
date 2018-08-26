207924240861e3075808458cecb8af25af41a9a3
kou <kosukeohen@icloud.com>
2018-08-26 16:16:59 +0000

    8 fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
-       redirect_to @user
-       flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
- end
+  end
 
   private
 
@@ -25,10 +25,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
-    
-  
-  
-  
-  
 end
